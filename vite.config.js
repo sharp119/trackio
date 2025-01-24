0aba8fa6495d10676268d1f99342fd452063e3c5
Aditya Paswan <cs.aditya58@gmail.com>
2025-01-24 15:39:14 +0530

    Prepare for production deployment

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,22 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    minify: 'terser',
+    chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
 })
